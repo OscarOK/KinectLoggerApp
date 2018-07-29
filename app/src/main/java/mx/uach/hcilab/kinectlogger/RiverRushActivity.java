268bf48f0912e40c4cf2dfe5b315f2b5471a5462
OscarOK <oscareom1a@gmail.com>
2018-07-29 02:11:16 -0600

    RiverRush buttons distribution done

--- app/src/main/java/mx/uach/hcilab/kinectlogger/RiverRushActivity.java
+++ app/src/main/java/mx/uach/hcilab/kinectlogger/RiverRushActivity.java
@@ -2,12 +2,27 @@ package mx.uach.hcilab.kinectlogger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class RiverRushActivity extends AppCompatActivity {
+
+    private ImageButton badJump;
+    private ImageButton inhibitionJump;
+    private ImageButton goodJump;
+    private ImageButton badMiddle;
+    private ImageButton inhibitionMiddle;
+    private ImageButton goodMiddle;
+    private ImageButton badLeft;
+    private ImageButton cloud;
+    private ImageButton badRight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_river_rush);
+
+        cloud = findViewById(R.id.river_rush_cloud);
     }
+
+
 }
