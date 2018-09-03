7b576095f43e9f32cad5d647b8685b6b958a71d6
Victor Aguilar <a320663@uach.mx>
2018-09-03 12:03:29 -0600

    pixelated images fixed

--- app/src/main/java/mx/uach/hcilab/kinectlogger/games/RallyBall.java
+++ app/src/main/java/mx/uach/hcilab/kinectlogger/games/RallyBall.java
@@ -7,6 +7,7 @@ import android.content.DialogInterface;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MotionEvent;
@@ -42,6 +43,7 @@ public class RallyBall extends AppCompatActivity implements LevelSelector.OnInpu
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rally_ball);
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
 
         gameTime = System.nanoTime();
 
